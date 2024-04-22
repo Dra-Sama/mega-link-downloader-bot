850a31bd8c8c980e5d043139f829955c93fbce74
Dragon <106417401+Dra-Sama@users.noreply.github.com>
2024-04-22 20:45:09 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,14 +14,11 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from pyrogram import Client, idle
 if __name__ == "__main__" :
     # Creating essential directories, if they does not exists
-    if:
-       not os.path.isdir(Config.DOWNLOAD_LOCATION):
+    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
-    elif: 
-        not os.path.isdir(Config.ADMIN_LOCATION):
+    if not os.path.isdir(Config.ADMIN_LOCATION):
         os.makedirs(Config.ADMIN_LOCATION)
-    else: 
-        not os.path.isdir(Config.CREDENTIALS_LOCATION):
+    if not os.path.isdir(Config.CREDENTIALS_LOCATION):
         os.makedirs(Config.CREDENTIALS_LOCATION)        
     plugins = dict(
         root="plugins"
